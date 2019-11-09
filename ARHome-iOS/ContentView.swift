977c616e0e01ed100bf2dec09759a87e40b664c0
gaojian <xiaoyaogaojian@outlook.com>
2019-11-09 20:54:18 +0800

    Initial Commit

---
+++ ARHome-iOS/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  ARHome-iOS
+//
+//  Created by 高健 on 2019/11/9.
+//  Copyright © 2019 高健. All rights reserved.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
